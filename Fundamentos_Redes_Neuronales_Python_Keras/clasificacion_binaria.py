e0351b0305826cc22302074373dd09629fdb6931
feltoxXx <5326671+feltoxXx@users.noreply.github.com>
2023-08-24 23:42:21 -0300

    menos complejidad, regularización, dropout

--- Fundamentos_Redes_Neuronales_Python_Keras/clasificacion_binaria.py
+++ Fundamentos_Redes_Neuronales_Python_Keras/clasificacion_binaria.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import imdb
 from keras import models, layers, optimizers
+from keras import regularizers
 
 # Descargamos los datos de imdb - Keras
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -30,43 +31,129 @@ y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
 
-# Creamos el modelo
-model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-
-# Compilamos el modelo
-model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-             metrics=['acc'])
-
-# Entrenamos el modelo
+# Preparamos la data
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 
 y_val = y_train[:10000]
 partial_y_train =  y_train[10000:]
 
-history = model.fit(partial_x_train,
+# Creamos el modelo 1
+model1 = models.Sequential()
+model1.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model1.add(layers.Dense(16, activation='relu'))
+model1.add(layers.Dense(1, activation='sigmoid'))
+
+# Compilamos el modelo 1
+model1.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+             metrics=['acc'])
+
+# Entrenamos el modelo 1
+history1 = model1.fit(partial_x_train,
                    partial_y_train,
                    epochs=20,
                    batch_size=512,
                    validation_data=(x_val,y_val))
 
+
+## Modelo menos complejo
+# Creamos el modelo 2
+model2 = models.Sequential()
+model2.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
+model2.add(layers.Dense(4, activation='relu'))
+model2.add(layers.Dense(1, activation='sigmoid'))
+
+# Compilamos el modelo 2
+model2.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+             metrics=['acc'])
+
+# Entrenamos el modelo 2
+history2 = model2.fit(partial_x_train,
+                   partial_y_train,
+                   epochs=20,
+                   batch_size=512,
+                   validation_data=(x_val,y_val))
+
+
+## Regularización
+# Creamos el modelo 3
+model3 = models.Sequential()
+model3.add(layers.Dense(16, activation='relu', input_shape=(10000,), kernel_regularizer=regularizers.l2(0.001)))
+model3.add(layers.Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+model3.add(layers.Dense(1, activation='sigmoid'))
+
+# Compilamos el modelo 3
+model3.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+             metrics=['acc'])
+
+# Entrenamos el modelo 3
+history3 = model3.fit(partial_x_train,
+                   partial_y_train,
+                   epochs=20,
+                   batch_size=512,
+                   validation_data=(x_val,y_val))
+
+## Dropout
+# Creamos el modelo 4
+model4 = models.Sequential()
+model4.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model4.add(layers.Dropout(0.5))
+model4.add(layers.Dense(16, activation='relu'))
+model4.add(layers.Dropout(0.5))
+model4.add(layers.Dense(1, activation='sigmoid'))
+
+# Compilamos el modelo 4
+model4.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+             metrics=['acc'])
+
+# Entrenamos el modelo 4
+history4 = model4.fit(partial_x_train,
+                   partial_y_train,
+                   epochs=20,
+                   batch_size=512,
+                   validation_data=(x_val,y_val))
+
+
 # Analizamos resultados
-history_dict = history.history
+history_dict = history1.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 
+val_loss_values2 = history2.history['val_loss']
+val_loss_values3 = history3.history['val_loss']
+val_loss_values4 = history4.history['val_loss']
+
+
 fig = plt.figure(figsize=(10,10))
 epoch = range(1,len(loss_values)+1)
-plt.plot(epoch,loss_values, 'o',label='training')
-plt.plot(epoch,val_loss_values, '--',label='val')
+plt.plot(epoch,val_loss_values2, 'o',label='smaller')
+plt.plot(epoch,val_loss_values, '--',label='original')
 plt.legend()
 plt.show()
 
-model.evaluate(x_test, y_test)
+fig = plt.figure(figsize=(10,10))
+epoch = range(1,len(loss_values)+1)
+plt.plot(epoch,val_loss_values3, 'o',label='regularization')
+plt.plot(epoch,val_loss_values, '--',label='original')
+plt.legend()
+plt.show()
+
+fig = plt.figure(figsize=(10,10))
+epoch = range(1,len(loss_values)+1)
+plt.plot(epoch,val_loss_values4, 'o',label='dropout')
+plt.plot(epoch,val_loss_values, '--',label='original')
+plt.legend()
+plt.show()
+
+
+model1.evaluate(x_test, y_test)
+model2.evaluate(x_test, y_test)
+model3.evaluate(x_test, y_test)
+model4.evaluate(x_test, y_test)
 
 
 # # Predicciones
