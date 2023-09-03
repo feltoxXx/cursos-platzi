40f62bfb02152afbc0369b553d1b46e21c5084fc
feltoxXx <5326671+feltoxXx@users.noreply.github.com>
2023-09-02 23:42:01 -0300

    Optimizando la red neuronal

--- Redes_Neuronales_Convolucionales_Python_Keras/cifar_clasificacion.py
+++ Redes_Neuronales_Convolucionales_Python_Keras/cifar_clasificacion.py
@@ -1,7 +1,6 @@
-from tensorflow import keras
 from keras.utils import to_categorical
-from keras import regularizers
-from keras.models import Sequential
+from keras import regularizers, optimizers
+from keras.models import Sequential, clone_model
 from keras.layers import Conv2D,MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
@@ -28,6 +27,13 @@ y_test = to_categorical(y_test, num_clases)
 
 # print(y_train[0])
 
+# Normalización
+mean = np.mean(x_train)
+std = np.std(x_train)
+
+x_train = (x_train - mean) / (std+1e-7)
+x_test = (x_test - mean) / (std+1e-7)
+
 # Dividimos el dataset para tener validación
 (x_train, x_valid) = x_train[5000:], x_train[:5000]
 (y_train, y_valid) = y_train[5000:], y_train[:5000]
@@ -46,31 +52,37 @@ model = Sequential()
 # Convolución 1
 model.add(Conv2D(base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer), input_shape = input_shape))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 
 # Convolución 2
 model.add(Conv2D(base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.2))
 
 # Convolución 3
 model.add(Conv2D(2*base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(Dropout(0.2))
 
 # Convolución 4
 model.add(Conv2D(2*base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.3))
 
 # Convolución 5
 model.add(Conv2D(4*base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 
 # Convolución 6
 model.add(Conv2D(4*base_filtros, (3,3), padding='same', kernel_regularizer=regularizers.l2(w_regularizer)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.4))
 
@@ -80,21 +92,51 @@ model.add(Dense(num_clases,activation='softmax'))
 
 model.summary()
 
+datagen = ImageDataGenerator(rotation_range=15,
+							width_shift_range=0.1,
+							height_shift_range=0.1,
+							horizontal_flip=True,
+							vertical_flip=True,
+							fill_mode='nearest',
+							brightness_range=[0.4,1.5])
+
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer=optimizers.Adam(),
               metrics=['accuracy'])
 
-checkpoint = keras.callbacks.ModelCheckpoint(filepath='cifar_clasificacion.hdf5',
-                                            verbose=1,
-                                            monitor='accuracy',
-                                            save_best_only=True)
+# model.compile(loss='categorical_crossentropy',
+#               optimizer='rmsprop',
+#               metrics=['accuracy'])
 
-hist = model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_valid, y_valid), verbose=2, callbacks=[checkpoint], shuffle=True)
+checkpoint = ModelCheckpoint(filepath='./models/best_model.hdf5',
+                             verbose=1,
+                             monitor='val_accuracy',
+                             save_best_only=True)
 
-plt.plot(hist.history['accuracy'],label='Train')
-plt.plot(hist.history['val_accuracy'],label='Valid')
-plt.legend()
-plt.show()
+hist = model.fit(datagen.flow(x_train, y_train, batch_size=128),
+                 callbacks=[checkpoint],
+                 steps_per_epoch=x_train.shape[0] // 128,
+                 epochs=50,
+                 verbose=2,
+                 validation_data=(x_valid, y_valid),
+                )
 
+
+# hist = model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_valid, y_valid), verbose=2, callbacks=[checkpoint], shuffle=True)
+
+# plt.plot(hist.history['accuracy'],label='Train')
+# plt.plot(hist.history['val_accuracy'],label='Valid')
+# plt.legend()
+# plt.show()
+
+
+# print('evaluate')
+# model.evaluate(x_test,y_test)
+
+
+# Clonamos el modelo
+model2 = clone_model(model)
+
+model2.load_weights('./models/best_model.hdf5')
 print('evaluate')
 model.evaluate(x_test,y_test)
